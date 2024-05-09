34251432ba969e4fcc8219cf87bf45b6e970c6b1
oscar <oscarmartinezb1996@gmail.com>
2024-05-09 15:58:10 +0200

    WIP

---
+++ src/CriteriaElasticsearchConverter.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Oscmarb\CriteriaElasticsearchConverter;
+
+use Oscmarb\Criteria\Criteria;
+use Oscmarb\Criteria\Filter\Condition\ConditionFilter;
+use Oscmarb\Criteria\Filter\Condition\FilterOperator;
+use Oscmarb\Criteria\Filter\Filter;
+use Oscmarb\Criteria\Filter\Logic\AndFilter;
+use Oscmarb\Criteria\Filter\Logic\OrFilter;
+use Oscmarb\Criteria\Order\CriteriaOrders;
+
+class CriteriaElasticsearchConverter
+{
+    private const RANGE_OPERATOR_CONVERSIONS = [
+        FilterOperator::LTE => 'lte',
+        FilterOperator::LT => 'lt',
+        FilterOperator::GTE => 'gte',
+        FilterOperator::GT => 'gt',
+    ];
+
+    private array $body;
+
+    /**
+     * @param array<string, string> $criteriaToMapFields
+     */
+    public static function create(string $index, array $criteriaToMapFields): self
+    {
+        return new self($index, $criteriaToMapFields);
+    }
+
+    /**
+     * @param array<string, string> $criteriaToMapFields
+     */
+    private function __construct(string $index, private array $criteriaToMapFields)
+    {
+        $this->body = ['index' => $index];
+    }
+
+    public function convert(Criteria $criteria): array
+    {
+        $filters = $criteria->filters()->values();
+
+        if (1 >= count($filters)) {
+            $filter = array_values($filters)[0] ?? null;
+        } else {
+            $filter = AndFilter::create(...$filters);
+        }
+
+        $offset = $criteria->offset();
+        $limit = $criteria->limit();
+
+        if (null !== $offset) {
+            $this->body['from'] = $offset->value();
+        }
+
+        if (null !== $limit) {
+            $this->body['size'] = $limit->value();
+        }
+
+        $query = $this->formatQuery($filter);
+
+        if (false === array_key_exists('bool', $query)) {
+            $query = ['bool' => ['must' => $query]];
+        }
+
+        return array_merge(
+            $this->body,
+            ['query' => $query],
+            $this->formatOrders($criteria->orders())
+        );
+    }
+
+    protected function formatQuery(Filter $filter): array
+    {
+        if (true === $filter instanceof AndFilter) {
+            return $this->formatAndFilter($filter);
+        }
+
+        if (true === $filter instanceof OrFilter) {
+            return $this->formatOrFilter($filter);
+        }
+
+        if (true === $filter instanceof ConditionFilter) {
+            return $this->formatConditionFilter($filter);
+        }
+
+        throw new \RuntimeException('Unknown filter type');
+    }
+
+    protected function formatAndFilter(AndFilter $filter): array
+    {
+        return [
+            'bool' => ['must' => array_map(fn(Filter $filter) => $this->formatQuery($filter), $filter->filters())],
+        ];
+    }
+
+    protected function formatOrFilter(OrFilter $filter): array
+    {
+        return [
+            'bool' => ['should' => array_map(fn(Filter $filter) => $this->formatQuery($filter), $filter->filters())],
+        ];
+    }
+
+    protected function formatConditionFilter(ConditionFilter $filter): array
+    {
+        $value = $filter->value()->value();
+        $field = $this->mapFieldValue($filter->field()->value());
+
+        if (true === $filter->operator()->isIn()) {
+            $this->ensureValueIsAnArray($value);
+
+            return [
+                'terms' => [$field => $value],
+            ];
+        }
+
+        if (true === $filter->operator()->isNotIn()) {
+            $this->ensureValueIsAnArray($value);
+
+            return [
+                'bool' => ['must_not' => ['terms' => [$field => $value]]],
+            ];
+        }
+
+        if (true === is_array($value)) {
+            throw new \RuntimeException('Unexpected array value');
+        }
+
+        if (true === $filter->operator()->isEqual()) {
+            if (null === $value) {
+                return ['bool' => ['must_not' => ['exists' => ['field' => $field]]]];
+            }
+
+            return ['term' => [$field => $value]];
+        }
+
+        if (true === $filter->operator()->isNotEqual()) {
+            if (null === $value) {
+                return ['exists' => ['field' => $field]];
+            }
+
+            return ['bool' => ['must_not' => ['term' => [$field => $value]]]];
+        }
+
+        if (null === $value) {
+            throw new \RuntimeException('Unexpected null value');
+        }
+
+        if (true === $this->isRangeOperator($filter)) {
+            return [
+                'range' => [$field => [self::RANGE_OPERATOR_CONVERSIONS[$filter->operator()->value()] => $value]],
+            ];
+        }
+
+        if (true === $filter->operator()->isContains()) {
+            return [
+                'wildcard' => [$field => "*$value*"],
+            ];
+        }
+
+        if (true === $filter->operator()->isEndsWith()) {
+            return [
+                'wildcard' => [$field => "$value*"],
+            ];
+        }
+
+        if (true === $filter->operator()->isStartsWith()) {
+            return [
+                'prefix' => [$field => $value],
+            ];
+        }
+
+        throw new \RuntimeException('Unexpected condition filter operator');
+    }
+
+    protected function formatOrders(CriteriaOrders $orders): array
+    {
+        if (true === $orders->isEmpty()) {
+            return [];
+        }
+
+        $indexedOrders = [];
+
+        foreach ($orders->values() as $order) {
+            $indexedOrders[] = [$this->mapFieldValue($order->orderBy()->value()) => ['order' => $order->orderType()->value()]];
+        }
+
+        return [
+            'sort' => $indexedOrders,
+        ];
+    }
+
+    private function mapFieldValue(string $value): string
+    {
+        return \array_key_exists($value, $this->criteriaToMapFields)
+            ? $this->criteriaToMapFields[$value]
+            : $value;
+    }
+
+    private function isRangeOperator(ConditionFilter $filter): bool
+    {
+        return array_key_exists($filter->operator()->value(), self::RANGE_OPERATOR_CONVERSIONS);
+    }
+
+    private function ensureValueIsAnArray(mixed $value): void
+    {
+        if (false === is_array($value)) {
+            throw new \RuntimeException('Operator value should receive an array value');
+        }
+    }
+}
---
+++ tests/CriteriaElasticsearchConverterTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Oscmarb\CriteriaElasticsearchConverter\Tests;
+
+use Oscmarb\Criteria\CriteriaBuilder;
+use Oscmarb\Criteria\Filter\Condition\ConditionFilterFactory;
+use Oscmarb\Criteria\Filter\Logic\OrFilter;
+use Oscmarb\CriteriaElasticsearchConverter\CriteriaElasticsearchConverter;
+use PHPUnit\Framework\TestCase;
+
+final class CriteriaElasticsearchConverterTest extends TestCase
+{
+    public function testShouldConvertCriteriaToSqlQuery(): void
+    {
+        $fieldMappings = [
+            'childField' => 'first_child_table.field',
+            'secondChildField' => 'second_child_table.field',
+            'oldOrder' => 'newOrder',
+        ];
+        $converter = CriteriaElasticsearchConverter::create('index_name', $fieldMappings);
+
+        $criteria = CriteriaBuilder::create()
+            ->addFilter(
+                OrFilter::create(
+                    ConditionFilterFactory::createEqual('childField', 'value'),
+                    ConditionFilterFactory::createEqual('secondChildField', null),
+                )
+            )
+            ->addFilter(ConditionFilterFactory::createNotEqual('oldOrder', null))
+            ->addFilter(ConditionFilterFactory::createIn('value', ['1', '2']))
+            ->addFilter(ConditionFilterFactory::createNotIn('value', [1, 2]))
+            ->addFilter(ConditionFilterFactory::createContains('value', 'value'))
+            ->addFilter(ConditionFilterFactory::createStartsWith('value', 'value'))
+            ->addFilter(ConditionFilterFactory::createEndsWith('value', 'value'))
+            ->addFilter(ConditionFilterFactory::createEqual('value', 'value'))
+            ->addFilter(ConditionFilterFactory::createNotEqual('value', 'value'))
+            ->addFilter(ConditionFilterFactory::createGt('value', 1))
+            ->addFilter(ConditionFilterFactory::createGte('value', 1))
+            ->addFilter(ConditionFilterFactory::createLt('value', 1))
+            ->addFilter(ConditionFilterFactory::createLte('value', 1))
+            ->addAscOrder('oldOrder')
+            ->addDescOrder('regularField')
+            ->setLimit(20)
+            ->setOffset(5)
+            ->createCriteria();
+
+        self::assertEquals(self::sanitizeJson($this->expectedJsonQuery()), self::sanitizeJson(\Safe\json_encode($converter->convert($criteria))));
+    }
+
+    private function expectedJsonQuery(): string
+    {
+        return '{
+  "index": "index_name", 
+  "from": 5,
+  "size": 20,
+  "query": {
+    "bool": {
+      "must": [
+        {"bool": {
+          "should": [
+            {"term": {"first_child_table.field": "value"}},
+            {"bool": {"must_not": {"exists": {"field": "second_child_table.field"}}}}
+          ]
+        }},
+        {"exists": {"field": "newOrder"}},
+        {"terms": {"value": ["1", "2"]}},
+        {"bool": {"must_not": {"terms": {"value": [1, 2]}}}},
+        {"wildcard": {"value": "*value*"}},
+        {"prefix": {"value": "value"}},
+        {"wildcard": {"value": "value*"}},
+        {"term": {"value": "value"}},
+        {"bool": {"must_not": {"term": {"value": "value"}}}},
+        {"range": {"value": {"gt": 1}}},
+        {"range": {"value": {"gte": 1}}},
+        {"range": {"value": {"lt": 1}}},
+        {"range": {"value": {"lte": 1}}}
+      ]
+    }
+  },
+  "sort": [
+    {"newOrder": {"order": "asc"}},
+    {"regularField": {"order": "desc"}}
+  ]
+}
+        ';
+    }
+
+    private static function sanitizeJson(string $json): string
+    {
+        return str_replace(["\n", "\t", ' '], '', $json);
+    }
+}
